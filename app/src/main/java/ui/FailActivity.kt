341812093a8063d0c3afb080b5361f5a981ef6df
shjung <gcjung777@gmail.com>
2023-04-19 20:16:49 +0900

    error

---
+++ app/src/main/java/ui/FailActivity.kt
@@ -0,0 +1,13 @@
+package ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sunflower_practice.R
+
+class FailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fail)
+        val a =
+    }
+}
