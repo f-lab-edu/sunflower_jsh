0fff6d9524d9577294b1af928b669ed238026bc9
shjung <gcjung777@gmail.com>
2023-04-19 19:53:20 +0900

    error test

---
+++ app/src/main/java/ui/FailActivity.java
@@ -0,0 +1,20 @@
+package ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sunflower_practice.R;
+
+public class FailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fail);
+        String a = "asdf";
+        a= 3;
+
+    }
+}
