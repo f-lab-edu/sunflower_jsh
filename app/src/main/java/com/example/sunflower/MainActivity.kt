79fbd114bc7a0ffabacb4bee9d7446491ac52780
shjung <gcjung777@gmail.com>
2023-04-22 22:16:09 +0900

    lint test

--- app/src/main/java/com/example/sunflower/MainActivity.kt
+++ app/src/main/java/com/example/sunflower/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
+        val st =
 
     }
 }
