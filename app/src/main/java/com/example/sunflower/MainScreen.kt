f54da40943ae0921a07a3927287c02279fd2b816
shjung <gcjung777@gmail.com>
2023-04-23 16:21:50 +0900

    하는김에 정리

--- app/src/main/java/com/example/sunflower/MainScreen.kt
+++ app/src/main/java/com/example/sunflower/MainScreen.kt
@@ -1,12 +1,9 @@
 package com.example.sunflower
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun MainScreen() {
-    Row { Text("아무것도")  }
 }
