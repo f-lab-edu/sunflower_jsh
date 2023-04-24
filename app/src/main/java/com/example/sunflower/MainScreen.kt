e0b0ea39ea17d96fc25bf7d2215c3bcabebd7a51
shjung <gcjung777@gmail.com>
2023-04-24 12:55:19 +0900

    Theme Test

--- app/src/main/java/com/example/sunflower/MainScreen.kt
+++ app/src/main/java/com/example/sunflower/MainScreen.kt
@@ -1,11 +1,9 @@
 package com.example.sunflower
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -15,17 +13,14 @@ fun MainScreen() {
 
     val systemUiController = rememberSystemUiController()
     systemUiController.setSystemBarsColor(
-        color = Color.Gray
+        color = MaterialTheme.colorScheme.primary
     )
-    ArtistCard()
+    ThemeTest()
 }
 
 @Composable
-fun ArtistCard() {
+fun ThemeTest() {
     Column {
-        Text("Alfred Sisley", color = MaterialTheme.colorScheme.primary)
-        Text("3 minutes ago")
-        Box {
-        }
+        Text("Theme Test", color = MaterialTheme.colorScheme.primary)
     }
 }
