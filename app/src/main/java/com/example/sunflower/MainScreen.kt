8f6303ff516b5203fe0338b6ba3a7863a9d4836c
shjung <gcjung777@gmail.com>
2023-04-26 15:31:22 +0900

    TopBar complete

--- app/src/main/java/com/example/sunflower/MainScreen.kt
+++ app/src/main/java/com/example/sunflower/MainScreen.kt
@@ -1,27 +1,25 @@
 package com.example.sunflower
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.TabRow
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.Tab
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TopAppBarDefaults.centerAlignedTopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.pagerTabIndicatorOffset
@@ -29,17 +27,13 @@ import com.google.accompanist.pager.rememberPagerState
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 
+@ExperimentalMaterial3Api
 @OptIn(ExperimentalPagerApi::class)
 @Preview
 @Composable
 fun MainScreen() {
     val pagerState = rememberPagerState()
     val coroutineScope = rememberCoroutineScope()
-
-    val systemUiController = rememberSystemUiController()
-    systemUiController.setSystemBarsColor(
-        color = MaterialTheme.colorScheme.background,
-    )
 
     val tabRowItems = listOf(
         TabRowItem(
@@ -54,32 +48,45 @@ fun MainScreen() {
         ),
     )
 
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = MaterialTheme.colorScheme.primary,
+    )
 
     Column {
 
-        TopAppBar(
+        CenterAlignedTopAppBar(
             modifier = Modifier.fillMaxWidth(),
-            backgroundColor = MaterialTheme.colorScheme.primary,
+            colors = centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            ),
             title = {
                 Text(
                     text = "Sunflower",
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .align(alignment = Alignment.CenterHorizontally),
-                    color = MaterialTheme.colorScheme.onPrimary,
+                        .width(IntrinsicSize.Min),
                     style = MaterialTheme.typography.titleLarge,
-                    textAlign = TextAlign.Center,
                 )
             },
+            actions = {
+                if (tabRowItems[pagerState.currentPage].title == "Plant list") {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_filter),
+                        contentDescription = null,
+                    )
+                }
+            }
         )
 
         TabRow(
             selectedTabIndex = pagerState.currentPage,
-            backgroundColor = MaterialTheme.colorScheme.primary,
+            backgroundColor = MaterialTheme.colorScheme.primaryContainer,
             indicator = { tabPositions ->
                 TabRowDefaults.Indicator(
                     Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
-                    color = MaterialTheme.colorScheme.onPrimary,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
                 )
             },
         ) {
@@ -90,12 +97,11 @@ fun MainScreen() {
                     icon = {
                         Icon(imageVector = ImageVector.vectorResource(id = item.icon), contentDescription = null)
                     },
-                    unselectedContentColor = MaterialTheme.colorScheme.onPrimary,
-                    selectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                    unselectedContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    selectedContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
                     text = {
                         Text(
                             text = item.title,
-                            color = MaterialTheme.colorScheme.onPrimary,
                             maxLines = 2,
                             overflow = TextOverflow.Ellipsis,
                         )
