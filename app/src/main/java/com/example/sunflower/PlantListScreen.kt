084e3110604ff97013b4cd942ad12f58efbba6c8
CalmCalm <shjung53>
2023-04-29 16:15:25 +0900

    trying to resolve reviewDog's comment

--- app/src/main/java/com/example/sunflower/PlantListScreen.kt
+++ app/src/main/java/com/example/sunflower/PlantListScreen.kt
@@ -28,8 +28,6 @@ fun PlantListScreen() {
             vertical = 8.dp,
             horizontal = 8.dp,
         ),
-                MaterialTheme.colorScheme.primaryContainer,
-            )
     ) {
         items(
             count = PlantDataObject.plantList.size,
