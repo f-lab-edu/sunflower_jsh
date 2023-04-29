17f7186d3f7c2ba33cdade2202084cb4dd306177
CalmCalm <shjung53>
2023-04-29 22:10:47 +0900

    리뷰독 코멘트 해결

--- app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
+++ app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
@@ -2,7 +2,12 @@ package com.example.sunflower
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -78,7 +83,7 @@ fun DetailInfoScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(12.dp)
+                .padding(12.dp),
         ) {
             Text(
                 text = "이름",
