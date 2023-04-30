45c609597f08440806e5d6173c10855e5a9e7f6c
shjung <gcjung777@gmail.com>
2023-04-30 13:35:18 +0900

    fab 구현

--- app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
+++ app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -26,18 +27,22 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@ExperimentalMaterial3Api
 @Preview
 @Composable
 fun DetailInfoScreen() {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(
                 MaterialTheme.colorScheme.primaryContainer,
-            )
-            .verticalScroll(rememberScrollState()),
+            ),
     ) {
-        Box() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+        ) {
             Image(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -49,76 +54,74 @@ fun DetailInfoScreen() {
                 contentDescription = null,
             )
 
-            FloatingActionButton(
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(8.dp),
-                onClick = { /*TODO*/ },
-                shape = CircleShape,
-                containerColor = MaterialTheme.colorScheme.secondary,
-                content = {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onSecondary,
-                    )
-                },
-            )
-            FloatingActionButton(
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(8.dp),
-                onClick = { /*TODO*/ },
-                shape = CircleShape,
-                containerColor = MaterialTheme.colorScheme.secondary,
-                content = {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_share),
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onSecondary,
-                    )
-                },
-            )
-        }
-        Column(
-            modifier = Modifier
+            Column(modifier = Modifier
                 .fillMaxSize()
-                .padding(12.dp),
-        ) {
-            Text(
-                text = "이름",
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .padding(vertical = 24.dp),
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = "Watering needs",
-                modifier = Modifier
-                    .align(CenterHorizontally),
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-            )
-            Text(
-                text = "water 21 days",
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .padding(bottom = 12.dp),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = "설명\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = "링크",
-                modifier = Modifier.padding(
-                    bottom = 64.dp,
-                ),
-            )
+                .padding(horizontal = 12.dp)) {
+                Text(
+                    text = "이름",
+                    modifier = Modifier
+                        .align(CenterHorizontally)
+                        .padding(vertical = 24.dp),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = "Watering needs",
+                    modifier = Modifier
+                        .align(CenterHorizontally),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
+                Text(
+                    text = "water 21 days",
+                    modifier = Modifier
+                        .align(CenterHorizontally)
+                        .padding(bottom = 12.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = "설명\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = "링크",
+                    modifier = Modifier.padding(
+                        bottom = 64.dp,
+                    ),
+                )
+            }
         }
+
+        FloatingActionButton(
+            modifier = Modifier
+                .padding(8.dp),
+            onClick = { /*TODO*/ },
+            shape = CircleShape,
+            containerColor = MaterialTheme.colorScheme.secondary,
+            content = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSecondary,
+                )
+            },
+        )
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(8.dp),
+            onClick = { /*TODO*/ },
+            shape = CircleShape,
+            containerColor = MaterialTheme.colorScheme.secondary,
+            content = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_share),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSecondary,
+                )
+            },
+        )
     }
 }
