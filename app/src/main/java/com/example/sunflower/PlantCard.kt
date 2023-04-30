41fcae292e157a5c5b3fceabd2dc57f4ed3b2697
shjung <gcjung777@gmail.com>
2023-04-30 21:53:31 +0900

    MyGarden에 추가 완료

--- app/src/main/java/com/example/sunflower/PlantCard.kt
+++ app/src/main/java/com/example/sunflower/PlantCard.kt
@@ -17,63 +17,65 @@ import androidx.compose.ui.unit.dp
 import com.example.sunflower.data.PlantViewData
 
 @Composable
-fun PlantCard(plantData: PlantViewData) {
-    Card(
-        shape = card,
-        modifier = Modifier.padding(
-            vertical = 8.dp,
-            horizontal = 8.dp,
-        ),
-    ) {
-        Column(
-            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+fun PlantCard(plantViewData: PlantViewData) {
+    if (plantViewData.isPlanted) {
+        Card(
+            shape = card,
+            modifier = Modifier.padding(
+                vertical = 8.dp,
+                horizontal = 8.dp,
+            ),
         ) {
-            PlantImage(plantData.image)
-            Text(
-                text = plantData.name,
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .padding(vertical = 12.dp),
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = stringResource(R.string.planted),
-                modifier = Modifier
-                    .align(CenterHorizontally),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-            )
-            Text(
-                text = plantData.plantedDate,
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .padding(bottom = 12.dp),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = stringResource(R.string.lastWatered),
-                modifier = Modifier
-                    .align(CenterHorizontally),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-            )
-            Text(
-                text = plantData.lastWateredDate,
-                modifier = Modifier
-                    .align(CenterHorizontally),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
-            Text(
-                text = "water in ${plantData.wateringCycle} days",
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .padding(bottom = 12.dp),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-            )
+            Column(
+                modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+            ) {
+                PlantImage(plantViewData.image)
+                Text(
+                    text = plantViewData.name,
+                    modifier = Modifier
+                        .align(CenterHorizontally)
+                        .padding(vertical = 12.dp),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = stringResource(R.string.planted),
+                    modifier = Modifier
+                        .align(CenterHorizontally),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
+                Text(
+                    text = plantViewData.plantedDate,
+                    modifier = Modifier
+                        .align(CenterHorizontally)
+                        .padding(bottom = 12.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = stringResource(R.string.lastWatered),
+                    modifier = Modifier
+                        .align(CenterHorizontally),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
+                Text(
+                    text = plantViewData.lastWateredDate,
+                    modifier = Modifier
+                        .align(CenterHorizontally),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Text(
+                    text = "water in ${plantViewData.wateringCycle} days",
+                    modifier = Modifier
+                        .align(CenterHorizontally)
+                        .padding(bottom = 12.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/sunflower/data/MockUpDataList.kt
+++ app/src/main/java/com/example/sunflower/data/MockUpDataList.kt
@@ -4,13 +4,13 @@ import com.example.sunflower.R
 
 object MockUpDataList {
     val plantList = listOf<PlantViewData>(
-        PlantViewData(R.drawable.img_apple, "이름", "3", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "4", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "5", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "6", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "7", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "8", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "9", "설명", "심은날짜", "물준날짜"),
-        PlantViewData(R.drawable.img_apple, "이름", "1", "설명", "심은날짜", "물준날짜"),
+        PlantViewData(R.drawable.img_apple, "이름1", "3", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름2", "4", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름3", "5", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름4", "6", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름5", "7", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름6", "8", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름7", "9", "설명", "심은날짜", "물준날짜", false, "출처"),
+        PlantViewData(R.drawable.img_apple, "이름8", "1", "설명", "심은날짜", "물준날짜", false, "출처"),
     )
 }
