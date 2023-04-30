819bdbc2ace028f4207d684fb76e95d6c1cc0679
shjung <gcjung777@gmail.com>
2023-04-30 14:01:22 +0900

    피드백 적용

---
+++ app/src/main/java/com/example/sunflower/MyGardenScreen.kt
@@ -0,0 +1,31 @@
+package com.example.sunflower
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.sunflower.data.MockUpDataList
+
+@Composable
+fun MyGardenScreen() {
+    LazyVerticalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.secondary),
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(
+            vertical = 8.dp,
+            horizontal = 8.dp,
+        ),
+    ) {
+        items(
+            count = MockUpDataList.plantList.size,
+            itemContent = { PlantCard(MockUpDataList.plantList[it]) },
+        )
+    }
+}
