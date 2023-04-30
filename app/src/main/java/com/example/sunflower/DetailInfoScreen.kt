67b3d3eda4f07b6b887aa14509f1bb809deea888
shjung <gcjung777@gmail.com>
2023-04-30 14:37:34 +0900

    정리

--- app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
+++ app/src/main/java/com/example/sunflower/DetailInfoScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.compose.ui.res.vectorResource
 
 @ExperimentalMaterial3Api
 @Preview
