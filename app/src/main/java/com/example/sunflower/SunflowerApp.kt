8a1b90c3ba65640be67161c18fc110c9bdd4f7ad
shjung <gcjung777@gmail.com>
2023-04-30 14:56:16 +0900

    merge

---
+++ app/src/main/java/com/example/sunflower/SunflowerApp.kt
@@ -0,0 +1,38 @@
+package com.example.sunflower
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@ExperimentalMaterial3Api
+@Composable
+fun SunflowerApp() {
+    val navController = rememberNavController()
+    AppNavHost(
+        navController = navController,
+    )
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "mainScreen",
+    ) {
+        composable(
+            "mainScreen",
+        ) {
+            MainScreen(onNavigateToDetail = { navController.navigate("detailInfoScreen") })
+        }
+
+        composable("detailInfoScreen") {
+            DetailInfoScreen()
+        }
+    }
+}
